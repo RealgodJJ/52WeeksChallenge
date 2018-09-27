be9fa5efd0f3b0eaa182ed60cd9430135c10145a
maaonan <xiaozhiqiu13@sina.com>
2018-09-27 09:07:57 +0800

    Use 'For' method.

--- money_challenge.py
+++ money_challenge.py
@@ -11,8 +11,8 @@ import math
 def main():
     # 每周的存入金额
     money_per_week = 10
-    # 记录周数
-    week = 1
+    # # 记录周数
+    # week = 1
     # 递增金额
     increase_pace = 10
     # 存钱计划周数
@@ -22,17 +22,16 @@ def main():
     # 记录每周存款数的列表
     money_list = []
 
-    while week <= 52:
-        saving += money_per_week
+    for week in range(total_week):
         money_list.append(money_per_week)
         saving = math.fsum(money_list)
 
         # 输出信息
-        print('第{}周：存入{}元，账户累计{}元'.format(week, money_per_week, saving))
+        print('第{}周：存入{}元，账户累计{}元'.format(week + 1, money_per_week, saving))
 
         # 更新下一周的存钱金额
         money_per_week += increase_pace
-        week += 1
+        # week += 1
 
 
 if __name__ == '__main__':
